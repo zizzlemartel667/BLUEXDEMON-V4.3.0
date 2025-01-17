706ed57043b526e9103d7c495662841ebfbcab87
Josefolajy <joseph17ola@gmail.com>
2025-01-18 00:04:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349163929054"
+global.pairingNumber = "2349021133263"
 
-global.botNumber = ["2349163929054"]
+global.botNumber = ["2349021133263"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
